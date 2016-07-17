63e530c34d741d9e1e0b6d0745ed06b24c975525
Ramon Lansangan <richersoon04@gmail.com>
2016-07-17 19:12:18 +0800

    first commit

---
+++ app/src/androidTest/java/com/example/rxworks/rxworksdemo/DemoTest.java
@@ -0,0 +1,16 @@
+package com.example.rxworks.rxworksdemo;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by richersoon on 7/17/16.
+ */
+public class DemoTest {
+
+    @Test
+    public void demoTest() {
+        assertEquals(1, 2);
+    }
+}
---
+++ app/src/test/java/com/example/rxworks/DemoTest.java
@@ -0,0 +1,16 @@
+package com.example.rxworks;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by richersoon on 7/17/16.
+ */
+public class DemoTest {
+
+    @Test
+    public void shouldThrowException() {
+        assertEquals(1, 2);
+    }
+}
